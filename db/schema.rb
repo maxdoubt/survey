8fce0667b24f938ae63a8629b527853bbad02155
maxdoubt <benjamin.racher@gmail.com>
2015-09-13 23:15:11 -0500

    postgres setup

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150914023933) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.text     "content"
     t.boolean  "yes_no"
