57e9925de0969445260af4f7d93a7e53c7bd58f8
rloftonevans <rloftonevans@gmail.com>
2015-10-26 18:20:50 +0000

    Removin game

--- app/assets/javascripts/group_vs_group.js
+++ app/assets/javascripts/group_vs_group.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1000, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
+// var game = new Phaser.Game(1000, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update });
 
 function preload() {
     game.load.image('phaser', '/sprites/phaser-dude.png');
